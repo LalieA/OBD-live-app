81ebeb142fc120a77664ff030d4daaef7edff4f9
QuentinBlsk <quentin.blasiak@gmail.com>
2022-12-12 15:03:50 +0100

    Ajout partie CAN

---
+++ arduino/main/can_config.h
@@ -0,0 +1,160 @@
+
+#define STANDARD_CAN_11BIT      1       // That depands on your car. some 1 some 0. 
+
+/* CAN ID PID */
+#if STANDARD_CAN_11BIT
+#define CAN_ID_PID          0x7DF
+#else
+#define CAN_ID_PID          0x18db33f1
+#endif
+
+/* filts and masks */
+#if STANDARD_CAN_11BIT
+unsigned long mask[4] = 
+{
+    0, 0x7FC,                // ext, maks 0
+    0, 0x7FC,                // ext, mask 1
+};
+unsigned long filt[12] = 
+{
+    0, 0x7E8,                // ext, filt 0
+    0, 0x7E8,                // ext, filt 1
+    0, 0x7E8,                // ext, filt 2
+    0, 0x7E8,                // ext, filt 3
+    0, 0x7E8,                // ext, filt 4
+    0, 0x7E8,                // ext, filt 5
+};
+#else
+unsigned long mask[4] =
+{
+    1, 0x1fffffff,               // ext, maks 0
+    1, 0x1fffffff,
+};
+unsigned long filt[12] =
+{
+    1, 0x18DAF110,                // ext, filt
+    1, 0x18DAF110,                // ext, filt 1
+    1, 0x18DAF110,                // ext, filt 2
+    1, 0x18DAF110,                // ext, filt 3
+    1, 0x18DAF110,                // ext, filt 4
+    1, 0x18DAF110,                // ext, filt 5
+};
+#endif
+
+/*  PIDs adresses
+    /!\ if you want to add a PID, you have to increase the MAX_PID and handle the conversion
+    Careful, the PID of the list of PID supported isn't count in MAX_PID
+    for doc : https://en.wikipedia.org/wiki/OBD-II_PIDs#Service_01_PID_03
+    */
+
+#define PID_SUPPORTED_1_20      0x00
+// 1 --> 20
+#define PID_ENGINE_LOAD         0x04
+#define PID_COOLANT_TEMP        0x05
+#define PID_FUEL_PRESSURE       0x0A
+#define PID_FUEL_ABS_PRESSURE   0x0B
+#define PID_ENGINE_PRM          0x0C
+#define PID_VEHICLE_SPEED       0x0D
+#define PID_TEMP_AIR_INTAKE     0x0F
+#define PID_AIR_FLOW_RATE       0x10
+#define PID_THROTTLE_POS        0x11
+
+#define PID_SUPPORTED_21_40     0x20
+// 21 --> 40
+#define PID_FUEL_REL_PRESSURE   0x22
+#define PID_FUEL_ABS_PRESSURE   0x23
+#define PID_FUEL_TANK_LVL       0x2F
+
+#define PID_SUPPORTED_41_60     0x40
+// 41--> 60
+#define PID_ABS_ENGINE_LOAD     0x43
+#define PID_AIR_FUEL_RATIO      0x44
+#define PID_REL_THROTTLE_POS    0x45
+#define PID_TEMP_AIR_AMBIENT    0x46
+#define PID_FUEL_TYPE           0x51
+#define PID_ETHANOL_FUEL        0x52
+#define PID_REL_ACC_PEDAL_POS   0x5A
+#define PID_OIL_TEMP            0x5C
+#define PID_ENGINE_FUEL_RATE    0x5E
+
+#define PID_SUPPORTED_61_80     0x60
+// 61 --> 80
+#define PID_DEMANDED_TORQUE     0x61
+#define PID_ACTUAL_TORQUE       0x62
+#define PID_REF_TORQUE          0x63
+#define PID_ENGINE_TORQUE_DATA  0x64
+
+/*  PIDs conversion, with units
+    for doc : https://en.wikipedia.org/wiki/OBD-II_PIDs#Service_01_PID_03
+    */
+
+#define PID_ENGINE_LOAD_CONVERSION(load)                    (load * 100) / 256                  // %
+#define PID_COOLANT_TEMP_CONVERSION(temp)                   (temp - 40)                         // °C
+#define PID_FUEL_PRESSURE_CONVERSION(press)                 (3 * press)                         // kPa
+#define PID_FUEL_ABS_PRESSURE_CONVERSION(press)             (press)                             // kPa
+#define PID_ENGINE_PRM_CONVERSION(rpm1, rpm2)               (rpm1 * 256 + rpm2) / 4             // rpm
+#define PID_VEHICLE_SPEED_CONVERSION(speed)                 (speed)                             // km/h
+#define PID_TEMP_AIR_INTAKE_CONVERSION(temp)                (temp - 40)                         // °C
+#define PID_AIR_FLOW_RATE_CONVERSION(rate1, rate2)          (256 * rate1 + rate2) / 100         // g/s
+#define PID_THROTTLE_POS_CONVERSION(throttle)               (throttle) * 100 / 256              // %
+
+#define PID_FUEL_REL_PRESSURE_CONVERSION(press1, press2)    (256 * press1 + press2) * 0.079     // kPa
+#define PID_FUEL_ABS_PRESSURE_CONVERSION(press1, press2)    (256 * press1 + press2) * 10        // kPa
+#define PID_FUEL_TANK_LVL_CONVERSION(lvl)                   (lvl * 100) / 256                   // %
+
+#define PID_ABS_ENGINE_LOAD_CONVERSION(load1, load2)        (256 * load1 + load2) * 100 / 256   // %
+#define PID_AIR_FUEL_RATIO_CONVERSION(ratio1, ratio2)       (256 * ratio1 + ratio2) * 2 / 65536 // ratio
+#define PID_REL_THROTTLE_POS_CONVERSION(pos)                (pos * 100) / 256                   // %
+#define PID_TEMP_AIR_AMBIENT_CONVERSION(temp)               (temp - 40)                         // °C
+#define PID_FUEL_TYPE_CONVERSION
+#define PID_ETHANOL_FUEL_CONVERSION(fuel)                   (fuel * 100) / 256                  // %
+#define PID_REL_ACC_PEDAL_POS_CONVERSION(pos)               (pos * 100) / 256                   // %
+#define PID_OIL_TEMP_CONVERSION(temp)                       (temp - 40)                         // °C
+#define PID_ENGINE_FUEL_RATE_CONVERSION(rate1, rate2)       (256 * rate1 + rate2) / 20          // L/h
+
+#define PID_DEMANDED_TORQUE_CONVERSION(torque)              (torque - 125)                      // %
+#define PID_ACTUAL_TORQUE_CONVERSION(torque)                (torque - 125)                      // %
+#define PID_REF_TORQUE_CONVERSION(torque1, torque2)         (256 * torque1 + torque2)           // Nm
+#define PID_ENGINE_TORQUE_DATA_CONVERSION
+
+/* PIDs tab */
+
+#define MAX_PID 25
+int pids[MAX_PID] = {
+    PID_ENGINE_LOAD,
+    PID_COOLANT_TEMP,
+    PID_FUEL_PRESSURE,
+    PID_FUEL_ABS_PRESSURE,
+    PID_ENGINE_PRM,
+    PID_VEHICLE_SPEED,
+    PID_TEMP_AIR_INTAKE,
+    PID_AIR_FLOW_RATE,
+    PID_THROTTLE_POS,
+
+    PID_FUEL_REL_PRESSURE,
+    PID_FUEL_ABS_PRESSURE,
+    PID_FUEL_TANK_LVL,
+
+    PID_ABS_ENGINE_LOAD,
+    PID_AIR_FUEL_RATIO,
+    PID_REL_THROTTLE_POS ,
+    PID_TEMP_AIR_AMBIENT,
+    PID_FUEL_TYPE,
+    PID_ETHANOL_FUEL,
+    PID_REL_ACC_PEDAL_POS,
+    PID_OIL_TEMP ,
+    PID_ENGINE_FUEL_RATE,
+
+    PID_DEMANDED_TORQUE,
+    PID_ACTUAL_TORQUE,
+    PID_REF_TORQUE,
+    PID_ENGINE_TORQUE_DATA
+};
+
+#define MAX_PID_SUPPORTED = 4
+int pids_supp[MAX_PID_SUPPORTED] = {
+    PID_SUPPORTED_1_20,
+    PID_SUPPORTED_21_40,
+    PID_SUPPORTED_41_60,
+    PID_SUPPORTED_61_80
+}
--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,123 +1,269 @@
-// #include <Serial_CAN_Module.h>
+#include <Serial_CAN_Module.h>
 
-// #include <SoftwareSerial.h>
-// #include "bluetooth_config.h"
-// #include "can_config.h"
+#include <SoftwareSerial.h>
+#include "bluetooth_config.h"
+#include "can_config.h"
 
-// // DEFINES
+// DEFINES
 
-// /*
-//   Wiring HC05 <---> Arduino UNO
-//      VCC <–> +5V
-//      GND <–> GND
-//      TXD <–> Pin 2 (RX)
-//      RXD <–> Pin 3 (TX) via a voltage divider bridge from 5V to 3.3V
-// */
+/*
+  Wiring HC05 <---> Arduino UNO
+     VCC <–> +5V
+     GND <–> GND
+     TXD <–> Pin 2 (RX)
+     RXD <–> Pin 3 (TX) via a voltage divider bridge from 5V to 3.3V
+*/
 
-// #define BLUETOOTH_RX 2
-// #define BLUETOOTH_TX 3
+#define BLUETOOTH_RX 2
+#define BLUETOOTH_TX 3
 
-// /*
-//   Wiring OBD-II <---> Arduino UNO
-//      VCC <–> +5V
-//      GND <–> GND
-//      TXD <–> Pin 4 (TX)
-//      RXD <–> Pin 5 (RX)
-// */
+/*
+  Wiring OBD-II <---> Arduino UNO
+     VCC <–> +5V
+     GND <–> GND
+     TXD <–> Pin 4 (TX)
+     RXD <–> Pin 5 (RX)
+*/
 
-// #define CAN_RX 4
-// #define CAN_TX 5
+#define CAN_RX 4
+#define CAN_TX 5
 
-// // VARIABLES
-// SoftwareSerial SerialBluetooth(BLUETOOTH_RX, BLUETOOTH_TX);
+// VARIABLES
+SoftwareSerial SerialBluetooth(BLUETOOTH_RX, BLUETOOTH_TX);
 
-// Serial_CAN can;
+Serial_CAN can;
+int pids_supp_mask[MAX_PID];
 
-// // PROTOTYPES
-// bool init_AT();
-// bool send_AT_cmd(const char * at_cmd);
+// PROTOTYPES
+bool init_AT();
+bool send_AT_cmd(const char * at_cmd);
 
-// bool init_CAN();
-// int convert_from_bytes_to_unite(int const id_pid, int const values1, int const values2);
-// void sendPid(unsigned char __pid);
-// bool getPid(unsigned char const __pid, int *value, int const nbr_of_bytes);
+bool init_CAN();
+bool init_PIDs();
+bool is_pid_supported(int id_pid, int pids_supp, int offset) ;
+int convert_from_bytes_to_unite(int const id_pid_index, int const values1, int const values2);
+void sendPid(unsigned char __pid);
+bool getPid(unsigned char const pid_id_index, int *value);
 
 
 
-// // FONCTIONS
-// /* BLUETOOTH */
+// FONCTIONS
+/* BLUETOOTH */
 
-// bool send_AT_cmd(const char * at_cmd) {
-//   String _recv = "";
-//   const char * recv;
+bool send_AT_cmd(const char * at_cmd) {
+  String _recv = "";
+  const char * recv;
   
-//   Serial.print(at_cmd);
-//   SerialBluetooth.write(at_cmd);
+  Serial.print(at_cmd);
+  SerialBluetooth.write(at_cmd);
   
-//   _recv = SerialBluetooth.readString();
-//   _recv.trim();
+  _recv = SerialBluetooth.readString();
+  _recv.trim();
   
-//   recv = _recv.c_str();
-//   const int len = _recv.length();
+  recv = _recv.c_str();
+  const int len = _recv.length();
 
-//   if(recv[len - 2] != 'O' || recv[len - 1] != 'K')
-//     return false;
-//   return true;
-// }
+  if(recv[len - 2] != 'O' || recv[len - 1] != 'K')
+    return false;
+  return true;
+}
 
-// bool init_AT() {
-//   Serial.print("-- AT config init --\n");
-//   String recv = "";
-//   uint8_t count_timeout = 0;
-//   bool isOK = false;
+bool init_AT() {
+  Serial.print("-- AT config init --\n");
+  String recv = "";
+  uint8_t count_timeout = 0;
+  bool isOK = false;
   
-//   while(isOK == false && count_timeout <= AT_INIT_TIMEOUT) {
-//     isOK = send_AT_cmd(AT);
-//     count_timeout++;
-//   }
+  while(isOK == false && count_timeout <= AT_INIT_TIMEOUT) {
+    isOK = send_AT_cmd(AT);
+    count_timeout++;
+  }
 
-//   if(isOK) {
-//     isOK &= send_AT_cmd(AT_VERSION);
-//     isOK &= send_AT_cmd(AT_NAME);
-//     isOK &= send_AT_cmd(AT_ROLE);
-//     isOK &= send_AT_cmd(AT_RESET);
-//   }
+  if(isOK) {
+    isOK &= send_AT_cmd(AT_VERSION);
+    isOK &= send_AT_cmd(AT_NAME);
+    isOK &= send_AT_cmd(AT_ROLE);
+    isOK &= send_AT_cmd(AT_RESET);
+  }
 
-//   if(!isOK)
-//     Serial.println("Errors occured.");
+  if(!isOK)
+    Serial.println("Errors occured.");
 
-//   Serial.print("-- AT config finished --");
-//   return isOK;
-// }
+  Serial.print("-- AT config finished --");
+  return isOK;
+}
 
-// /* OBD-II */
+/* OBD-II */
 
-// bool init_CAN() {
-//   can.begin(CAN_TX, CAN_RX, 38400);
-//   Serial.println("CAN transmission ok to start");
-//   return true;
-// }
+bool init_CAN() {
+  can.begin(can_tx, can_rx, 38400);      // tx, rx
 
-// /* MAIN */
+  Serial.println(can.canRate(CAN_RATE_500) ? "set can rate ok" : "set can rate fail");
+  Serial.println(can.setMask(mask) ? "set mask ok" : "set mask fail");
+  Serial.println(can.setFilt(filt) ? "set filt ok" : "set filt fail");
+  return true;
+}
 
-// // void setup()
-// {
-//   Serial.begin(115200);
-//   SerialBluetooth.begin(38400);
+// TODO a tester 
+bool init_PIDs() {
+    int pids_supp_1_20;
+    getPid(pid_supp[0], pids_supp_1_20);
+    int pids_supp_21_40;
+    getPid(pid_supp[0], pids_supp_21_40);
+    int pids_supp_41_60;
+    getPid(pid_supp[0], pids_supp_41_60);
+    int pids_supp_61_80;
+    getPid(pid_supp[0], pids_supp_61_80);
 
-//   Serial.print("Sketch:   ");   Serial.println(__FILE__);
-//   Serial.print("Uploaded: ");   Serial.println(__DATE__);
-//   Serial.println("READY");
+    for (int pid_id; pid_id < MAX_PID; pid_id++){
+        if (pids[pid_id] <= 0x20)
+        {
+            pids_supp_mask[pid_id] = is_pid_supported(pid_id, pids_supp_1_20, 0x0);
+        }
+        else if (pids[pid_id] <= 0x40)
+        {
+            pids_supp_mask[pid_id] = is_pid_supported(pid_id, pids_supp_21_40, 0x20);
+        }
+        else if (pids[pid_id] <= 0x60)
+        {
+            pids_supp_mask[pid_id] = is_pid_supported(pid_id, pids_supp_41_60, 0x40);
+        }
+        else if (pids[pid_id] <= 0x80)
+        {
+            pids_supp_mask[pid_id] = is_pid_supported(pid_id, pids_supp_61_80, 0x60);
+        }
+        else
+        {
+            Seriel.println("The id proposed in pid supported checking doesn't exist")
+        }
+    }
+}
 
-//   init_AT();
-//   init_CAN();
-// }
-// void loop()
-// {
-//   if (SerialBluetooth.available()) {
-//     Serial.write(SerialBluetooth.read());
-//   }
-//   if (Serial.available()) {
-//     SerialBluetooth.write(Serial.read());
-//   }
-// }
+bool is_pid_supported(int id_pid, int pids_supp, int offset) {
+    
+    return (pids_supp >> (0x20 + offset - id_pid)) & 0x1;
+}
+
+void sendPid(unsigned char __pid) {
+    unsigned char tmp[8] = {0x02, 0x01, __pid, 0, 0, 0, 0, 0};
+
+#if STANDARD_CAN_11BIT
+    can.send(CAN_ID_PID, 0, 0, 8, tmp);   // SEND TO ID:0X55
+#else
+    can.send(CAN_ID_PID, 1, 0, 8, tmp);   // SEND TO ID:0X55
+#endif
+}
+
+bool getPid(unsigned char const pid_id_index, int *value)
+{
+    sendPid(pids[pid_id_index]);
+    unsigned long __timeout = millis();
+
+    while(millis() - __timeout < 3000)  // 1s time out
+    {
+        unsigned long id = 0;
+        unsigned char buf[8];
+
+        if (can.recv(&id, buf)) {  // check if get data
+            if(buf[1] == 0x41)
+            { 
+                *value = convert_from_bytes_to_unite(pid_id_index, buf[3], buf[4]);       
+                return 1;
+            }
+        }
+    }
+    Serial.println("timed out");    
+    return 0;
+}
+
+int convert_from_bytes_to_unite(int const id_pid, int const values1, int const values2)
+{
+    switch (id_pid)
+    {
+    case 0: //PID_ENGINE_LOAD:
+        return PID_ENGINE_LOAD_CONVERSION(values1);
+    case 1: //PID_COOLANT_TEMP:
+        return PID_COOLANT_TEMP_CONVERSION(values1, values2);
+    case 2: //PID_FUEL_PRESSURE:
+        return PID_FUEL_PRESSURE_CONVERSION(values1);
+    case 3: //PID_FUEL_ABS_PRESSURE:
+        return PID_FUEL_ABS_PRESSURE_CONVERSION(values1, values2);
+    case 4: //PID_ENGINE_PRM:
+        return PID_ENGINE_PRM_CONVERSION(values1, value2);
+    case 5: //PID_VEHICLE_SPEED:
+        return PID_VEHICLE_SPEED_CONVERSION(values1);
+    case 6: //PID_TEMP_AIR_INTAKE:
+        return PID_TEMP_AIR_INTAKE_CONVERSION(values1);
+    case 7: //PID_AIR_FLOW_RATE:
+        return PID_AIR_FLOW_RATE_CONVERSION(values1, value2);
+    case 8: //PID_THROTTLE_POS:
+        return PID_THROTTLE_POS_CONVERSION(values1);
+
+    case 9: //PID_FUEL_REL_PRESSURE:
+        return PID_FUEL_REL_PRESSURE_CONVERSION(values1, value2);
+    case 10: //PID_FUEL_ABS_PRESSURE:
+        return PID_FUEL_ABS_PRESSURE_CONVERSION(values1, value2);
+    case 11: //PID_FUEL_TANK_LVL:
+        return PID_FUEL_TANK_LVL_CONVERSION(values1);
+
+    case 12: //PID_ABS_ENGINE_LOAD:
+        return PID_ABS_ENGINE_LOAD_CONVERSION(values1, value2);
+    case 13: //PID_AIR_FUEL_RATIO:
+        return PID_AIR_FUEL_RATIO_CONVERSION(values1, value2);
+    case 14: //PID_REL_THROTTLE_POS:
+        return PID_REL_THROTTLE_POS_CONVERSION(values1);
+    case 15: //PID_TEMP_AIR_AMBIENT:
+        return PID_TEMP_AIR_AMBIENT_CONVERSION(values1);
+    case 16: //PID_FUEL_TYPE:
+        return PID_FUEL_TYPE_CONVERSION;
+    case 17: //PID_ETHANOL_FUEL:
+        return PID_ETHANOL_FUEL_CONVERSION(values1);
+    case 18: //PID_REL_ACC_PEDAL_POS:
+        return PID_REL_ACC_PEDAL_POS_CONVERSION(values1);
+    case 19: //PID_OIL_TEMP:
+        return PID_OIL_TEMP_CONVERSION(values1);
+    case 20: //PID_ENGINE_FUEL_RATE:
+        return PID_ENGINE_FUEL_RATE_CONVERSION(values1, values2);
+    
+    case 21: //PID_DEMANDED_TORQUE:
+        return PID_DEMANDED_TORQUE_CONVERSION(values1);
+    case 22: //PID_ACTUAL_TORQUE:
+        return PID_ACTUAL_TORQUE_CONVERSION(values1);
+    case 23: //PID_REF_TORQUE:
+        return PID_REF_TORQUE_CONVERSION(values1, values2);
+    case 24: //PID_ENGINE_TORQUE_DATA:
+        return PID_ENGINE_TORQUE_DATA_CONVERSION;
+    default:
+        Serial.println("Error : pid requested doesn't exist");
+        break;
+    }
+    return 0;
+}
+
+/* MAIN */
+
+void setup()
+{
+  Serial.begin(115200);
+  SerialBluetooth.begin(38400);
+
+  Serial.print("Sketch:   ");   Serial.println(__FILE__);
+  Serial.print("Uploaded: ");   Serial.println(__DATE__);
+  Serial.println("READY");
+
+  init_AT();
+  init_CAN();
+  init_PIDs();
+  //TODO send on bluetooth pids_supp_mask.
+}
+
+void loop()
+{
+  //TODO send the values a cording to the 2 pids tabs (id and supp)
+  if (SerialBluetooth.available()) {
+    Serial.write(SerialBluetooth.read());
+  }
+  if (Serial.available()) {
+    SerialBluetooth.write(Serial.read());
+  }
+}
